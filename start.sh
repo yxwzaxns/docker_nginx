989d92cecbf97f4bdebaa0859316b6c2c474072e
yxwzaxns <yxwzaxns@gmail.com>
2015-12-03 12:06:12 +0800

    aa

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash --login
 sed -i "s/influxdb/${INFLUXDB_PORT_8083_TCP_ADDR}/" /etc/nginx/conf.d/default.conf
+sed -i "s/influxport/${INFLUXDB_PORT_8083_TCP_PORT}/" /etc/nginx/conf.d/default.conf
 service nginx start
 curl $INFLUXDB_PORT_8083_TCP_ADDR:8083
 tail -f /var/log/nginx/error.log
